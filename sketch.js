8011a6858104f47b3a3c446fe3a95f3f5207bd6e
pastagirl7 <142358367+pastagirl7@users.noreply.github.com>
2023-10-10 14:09:24 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,93 +1,87 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-let engine;
-let world;
-var angle=60;
+var ball,groundObj,leftSide,rightSide;
+var world;
+var radius = 70;
 
-var ground;
-var b1,b2,b3,b4;
-var top_wall;
-var ball,rock;
+function preload(){
 
-var btn1;
-var btn2;
+	dustbinImg = loadImage("dustbin.png");
+	paperImg = loadImage("paper.png");
+
+	//dustbinImg = addImage("dustbin.png");
+	//paperImg = addImage("paper.png");
+
+	//dustbin.loadImage("dustbin.png");
+	//paper.addImage("paper.png");
+	
+	//dustbin.loadImage("dustbin.png");
+	//paper.loadImage("paper.png");
+}
+
+
 function setup() {
-  createCanvas(400,400);
+	createCanvas(1600, 700);
+	rectMode(CENTER);
 
-  engine = Engine.create();
-  world = engine.world;
+	engine = Engine.create();
+	world = engine.world;
+
+	var ball_options={
+		isStatic:false,
+		restitution:0.3,
+		density:0.4
+	}
+
+	ball = Bodies.circle(260,100,radius/2.6,ball_options);
+	World.add(world,ball);
+
+	ground=new Ground(width/2,670,width,20);
+	leftSide = new Ground(1100,600,10,120);
+	rightSide = new Ground(1270,600,10,120);
+	bottomSide = new Ground(1185,650,150,20);
+
+	Engine.run(engine);
   
-   
-  var ground_options ={
-    isStatic: true
-  };
- 
-  var ball_options = {
-    // restitution: 0.2,
-    // restitution: 1,
-    // restitution: -0.8,
-     restitution: 0.75,
-  }
-
-  var rock_options = {
-     restitution: 0.2,
-     //restitution: 1,
-     //restitution: -0.8,
-    // restitution: 0.75,
-
-  }
-   
-  btn2 = createImg('up.png');
-  btn2.position(350,30);
-  btn2.size(50,50);
-  btn2.mouseClicked(vForce);
-  
-  ball = Bodies.circle(100,10,20,ball_options);
-  World.add(world,ball);
-  
-  rock = Bodies.circle(250,10,20,rock_options);
-  World.add(world,rock);
-
-  ground= Bodies.rectangle(200,390,400,20,ground_options);
-
-  World.add(world, ground);
- 
-  
-
-  rectMode(CENTER);
-  ellipseMode(RADIUS);
 }
 
 
-function draw() 
-{
-  background(51);
-  Engine.update(engine);
-  
-rect(ground.position.x,ground.position.y,400,20);
-ellipse(ball.position.x,ball.position.y,20);
-push();
-fill("brown");
-ellipse(rock.position.x,rock.position.y,20);
-pop();
+function draw() {
+	background(200);
+	rectMode(CENTER);
+
+
+	ground.display();
+	leftSide.display();  
+	rightSide.display();
+	bottomSide.display();
+
+	
+	imageMode(CENTER);
+
+	//image(paperImg,ball.position.y,ball.position.x,radius,radius);
+	//image(paperImg,ball.position.x,ball.position.y,radius/2,radius/2);
+	//ellipse(ball.position.x,ball.position.y,radius,radius);
+	image(paperImg,ball.position.x,ball.position.y,radius,radius);
+
+
+	
+	//image(1185, 570, 200,200);
+	//rect(1185, 570, 200,200);
+	image(dustbinImg, 1185, 570, 200,200);
+	//ellipse(1185, 570, 200,200)
+
+
+
 }
 
-function vForce()
-{
-   Matter.Body.applyForce(ball,{x:0,y:0},{x:0,y:-0.05});
+function keyPressed() {
+  	if (keyCode === UP_ARROW) {
 
-  // Matter.Body.applyForce(ball,{x:0,y:0},{x:0,y:0});
-
-  // Matter.Body.applyForce(ball,{x:0,y:0},{x:0.5,y:0});
-
-  // Matter.Body.applyForce(ball,{x:0,y:0},{x:0,y:0.05});
+		Matter.Body.applyForce(ball,ball.position,{x:85,y:-85});
+    
+  	}
 }
-
-
-  
-
-
